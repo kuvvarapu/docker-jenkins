b6c1c7ec0110d0b0bbbe494f8ea9a8cf144edd7c
Kuvvarapu <kuvvarapu@Kuvvarapus-MacBook-Pro.local>
2018-06-28 17:22:46 +0530

    branchcheck

--- CI-CD/branchcheck.sh
+++ CI-CD/branchcheck.sh
@@ -2,9 +2,9 @@
 #Description: This script is used to check the brach
 result=`git branch|tail -1`
 git branch
-if [ $result == developer ]
+if [ $result == eveloper ]
 then
-   echo "0"
+   echo "success"
  else
-   echo "1"
+   echo "error"
 fi
